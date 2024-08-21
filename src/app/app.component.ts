c85f9b2a2335ad57c1dc331e6e79ae4f1bf81b39
saibel203 <saibl.og203@gmail.com>
2024-08-21 15:14:20 +0300

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+  ],
 })
 export class AppComponent {
   title = 'MusShop.Client';
