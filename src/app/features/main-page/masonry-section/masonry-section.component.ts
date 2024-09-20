45da3c8d9b6b4413dd80b05ea98d245104783773
saibel203 <saibl.og203@gmail.com>
2024-09-20 10:37:24 +0300

    Start masonry

---
+++ src/app/features/main-page/masonry-section/masonry-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-masonry-section',
+  templateUrl: './masonry-section.component.html',
+  styleUrl: './masonry-section.component.scss',
+  standalone: true,
+  imports: [
+  ],
+})  
+export class MasonrySectionComponent {
+}
